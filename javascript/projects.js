88f5a8fddb13abb136612c3224d42931a02010c3
umekow <umekolashun@gmail.com>
2020-05-22 14:06:08 -0500

    added each project

--- javascript/projects.js
+++ javascript/projects.js
@@ -1,4 +1,4 @@
-
+import skills from './skills.js'; 
 const projects_section = document.querySelector('.projects-container'); 
 
 
@@ -13,7 +13,7 @@ const project_list = [
         'title': 'Citrics.io', 
         'img': '../images/projects/citrics/dashboard_monitor.png', 
         'github': 'https://github.com/umekow/city-data-comparison-fe', 
-        'stack': 'HTML, CSS, JavaScript, React, Nodejs', 
+        'stack': [ 'Html', 'CSS', 'JavaScript', 'React', 'Nodejs'], 
         'link': 'https://v1.citrics.io', 
         'summary':'React web application that helps users make informed decisions about where they want to relocate to' , 
         'bullets': ['Solely responsible for building and designing the signup and login pages', 'Collaborated with 7 team members on a cross functional team to complete project in 5-week deadline'], 
@@ -23,7 +23,7 @@ const project_list = [
         'title': 'Bountiful ', 
         'img': '../images/projects/bountiful/campaigns_monitor.png', 
         'github': 'https://github.com/bw-donor-mgmt/back-end/', 
-        'stack': 'HTML, CSS, JavaScript, React', 
+        'stack': [ 'Html', 'CSS', 'JavaScript', 'React'], 
         'link': 'https://front-end-mlqg5zp0w.now.sh/', 
         'summary':'React application built to help users organize contact information for all donors to their organization\'s campaigns' , 
         'bullets': ['solely responsible for developing the "about us" page, navigation, and header portion of the landing page', 'Held the role of senior front-end developer and collaborated with teams members to build front end'], 
@@ -33,7 +33,7 @@ const project_list = [
         'title': 'Empathy Builder', 
         'img': '../images/projects/eb/landing_monitor.png', 
         'github': 'https://github.com/DV-Empathy-Builder/landing-page-merge-fix', 
-        'stack': 'HTML, CSS, JavaScript', 
+        'stack': [ 'Html', 'CSS', 'JavaScript'], 
         'link': 'https://dv-empathy-builder.github.io/landing-page-merge-fix/', 
         'summary':'Helps web page visitors build empathy for victims of domestic violence by calculating the cost to relocate' , 
         'bullets': ['Solely responsible for constructing the navigation and about us page', 'Collaborated with team members to build landing pages for project'],
@@ -58,10 +58,19 @@ function createNewProject(project){
     img.alt = project.title; 
     project_container.appendChild(img); 
 
+    //tech stack
+
+    const stack_container = document.createElement('div'); 
+    project_container.appendChild(stack_container); 
+
+    project.stack.forEach(skill => project_container.insertAdjacentHTML('beforeend', `<i class="${skills[skill]} fa-2x"></i>`)); 
+
+    //project summary 
     const summary = document.createElement('p'); 
     summary.textContent = project.summary; 
     project_container.appendChild(summary); 
 
+    //bullet points 
     const bullet_points = document.createElement('ul'); 
     project.bullets.forEach(bullet =>{ 
         const point = document.createElement('li'); 
