19eb691eb201b3b379dee0a870be9567e1c7c052
umekow <umekolashun@gmail.com>
2020-05-21 22:34:10 -0500

    convert elements in skill list to objects to keep track of classes for fontawesome icons; commented the skills that do not have any icons

--- javascript/skills.js
+++ javascript/skills.js
@@ -1,15 +1,36 @@
 const skills_div = document.querySelector('.skills-container'); 
 
-const skills_list = ['Html', 'CSS', 'JavaScript', 'Python', 'Java', 'React', 'Node.js', 'Express', 'REST', 'Redux', 'LESS', 'SASS', 'Knex', 'SQL', 'Ant.Design', 'Android', 'Postgres' ]
+const skills_list = [  
+    {'skill':'Html', 'class':'fab fa-html5'}, 
+    {'skill':'CSS', 'class':'fab fa-css3-alt'}, 
+    {'skill':'JavaScript', 'class':'fab fa-js-square'}, 
+    {'skill':'Python', 'class':'fab fa-python'}, 
+    {'skill':'Java', 'class':'fab fa-java'}, 
+    {'skill':'React', 'class':'fab fa-java'}, 
+    {'skill':'Node.js', 'class':'fab b fa-javanode-js'}, 
+    //{'skill':'Express', 'class':'fab'}, 
+    //{'skill':'REST', 'class':'fab'}, 
+    //{'skill':'Redux', 'class':'fab'}, 
+    //{'skill':'LESS', 'class':'fab'}, 
+    {'skill':'SASS', 'class':'fab fa-sass'}, 
+    {'skill':'Knex', 'class':'fab fa-database'}, 
+    {'skill':'SQL', 'class':'fab fa-database'}, 
+    //{'skill':'Ant.Design', 'class':'fab'}, 
+    {'skill':'Android', 'class':'fab fa-android'}, 
+    {'skill':'Postgres', 'class':'fab fa-database'}, 
+]
 
 function newSkill(skill){
     const skill_container = document.createElement('div'); 
     skill_container.classList.add('skill'); 
+    const icon = document.createElement('i')
+
     const skill_text = document.createElement('p'); 
     skill_text.textContent = skill; 
     skill_container.appendChild(skill_text); 
 
     return skill_container
 }
+
 
 skills_list.forEach(skill => skills_div.appendChild(newSkill(skill)))
