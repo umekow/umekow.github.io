60f5d105a990273ecd1b68a5255c5e45b375006a
umekow <umekolashun@gmail.com>
2020-05-22 10:39:22 -0500

    added images for projects

---
+++ javascript/projects.js
@@ -0,0 +1,27 @@
+
+const projects_section = document.querySelector('#projects'); 
+
+
+/*
+summary : one sentence
+responsiblities: 2 - 3 bullet points '
+
+*/
+
+const projects = [
+    {
+        'title': 'Citrics.io', 
+        'img': '../images/citrics/', 
+        'github': , 
+        'stack': , 
+        'link': , 
+        'summary': , 
+    }
+]
+function createNewProject(project){
+    const project = document.createElement('div')
+
+    return project
+}
+
+
