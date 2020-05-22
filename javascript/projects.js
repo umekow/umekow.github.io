4073c28ab23232388e6381de85c6ba49e0e27403
umekow <umekolashun@gmail.com>
2020-05-22 12:04:45 -0500

    added object for each project

--- javascript/projects.js
+++ javascript/projects.js
@@ -11,11 +11,29 @@ responsiblities: 2 - 3 bullet points '
 const projects = [
     {
         'title': 'Citrics.io', 
-        'img': '../images/citrics/', 
-        'github': , 
-        'stack': , 
-        'link': , 
-        'summary': , 
+        'img': '../images/citrics/dashboard_monitor.png', 
+        'github': 'https://github.com/umekow/city-data-comparison-fe', 
+        'stack': 'HTML, CSS, JavaScript, React, Nodejs', 
+        'link': 'v1.citrics.io', 
+        'summary':'React web application that helps users make informed decisions about where they want to relocate to' , 
+    }, 
+
+    {
+        'title': 'Bountiful ', 
+        'img': '../images/bountiful/campaigns_monitor.png', 
+        'github': 'https://github.com/bw-donor-mgmt/back-end/', 
+        'stack': 'HTML, CSS, JavaScript, React', 
+        'link': 'https://front-end-mlqg5zp0w.now.sh/', 
+        'summary':'React application built to help users organize contact information for all donors to their organization\'s campaigns' , 
+    }, 
+
+    {
+        'title': 'Empathy Builder', 
+        'img': '../images/eb/landing_monitor.png', 
+        'github': 'https://github.com/DV-Empathy-Builder/landing-page-merge-fix', 
+        'stack': 'HTML, CSS, JavaScript', 
+        'link': 'https://dv-empathy-builder.github.io/landing-page-merge-fix/', 
+        'summary':'Helps web page visitors build empathy for victims of domestic violence by calculating the cost to relocate' , 
     }
 ]
 function createNewProject(project){
